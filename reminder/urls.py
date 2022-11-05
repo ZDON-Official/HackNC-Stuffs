66cc8722c37e34aa7b988a11839b163ab7c80cda
Zohaib <zrana1214@gmail.com>
2022-11-05 15:47:06 -0400

    folder renaming

--- reminder/urls.py
+++ reminder/urls.py
@@ -4,7 +4,7 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path(
         "",
-        TemplateView.as_view(template_name = "cards/base.html"),
+        TemplateView.as_view(template_name = "reminder/base.html"),
         name = "home"
     ),
 ]
